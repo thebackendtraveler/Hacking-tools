b398bf38fd5749cea85bb58fab531270f9f8770c
thebackendtraveler <anddyb671@gmail.com>
2023-05-15 03:22:05 -0400

    Modified the file

--- network_mapper.py
+++ network_mapper.py
@@ -28,16 +28,16 @@ cprint(F'\n* Green -> No error.                          *', 'green')
 # Regular Expression Pattern to recognise IPv4 addresses.
 ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
 
+def mapper(ip_add_range_entered):
+    try:
+        ip_add_range_entered = input("\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ")
+        if ip_add_range_pattern.search(ip_add_range_entered):
+            cprint(f"{ip_add_range_entered} is a valid ip address range", 'green')
+    except:
+        cprint(f'\n* Something went wrong, please try again  *', 'red')
 
-try:
-    ip_add_range_entered = input("\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ")
-    if ip_add_range_pattern.search(ip_add_range_entered):
-        cprint(f"{ip_add_range_entered} is a valid ip address range", 'green')
-except:
-    cprint(f'\n* Something went wrong, please try again  *', 'red')
-
-try:
-    arp_result = scapy.arping(ip_add_range_entered)
-    cprint(arp_result, 'green')
-except OSError as e:
-    cprint(f'\n* There was an error, please try again   *', 'red')
+    try:
+        arp_result = scapy.arping(ip_add_range_entered)
+        cprint(arp_result, 'green')
+    except OSError as e:
+        cprint(f'\n* There was an error, please try again   *', 'red')
