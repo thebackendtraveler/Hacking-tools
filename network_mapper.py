09a36c8f845af17efb7dfd0251652b9fc5599902
thebackendtraveler <anddyb671@gmail.com>
2023-05-16 02:48:28 -0400

    modified the file

--- network_mapper.py
+++ network_mapper.py
@@ -61,22 +61,21 @@ for host in nmScan.all_hosts():
          print('Protocol : %s' % proto)
  
          lport = nmScan[host][proto].keys()
-         lport.sort()
          for port in lport:
              print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state']))
 
 
 
 #try:
-for host in nmScan.all_hosts():
-    print('Host : %s (%s)' % (host, nmScan[host].hostname()))
-    print('State : %s' % nmScan[host].state())
-for proto in nmScan[host].all_protocols():
-    print('----------')
-    print('Protocol : %s' % proto)
+#for host in nmScan.all_hosts():
+    #print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+    #print('State : %s' % nmScan[host].state())
+    #for proto in nmScan[host].all_protocols():
+        #print('----------')
+        #print('Protocol : %s' % proto)
  
-lport = nmScan[host][proto].keys()
-for port in lport:
-    print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state']))
+    #lport = nmScan[host][proto].keys()
+    #for port in lport:
+        #print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state']))
 #except:
     #cprint(f'\nThere was an error happening, please try again..', 'red')
