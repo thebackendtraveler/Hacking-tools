1a3809cfa6ea5f7b38c8eb313dd2f41a1915342a
Andrea Dybendal <anddyb671@gmail.com>
2023-05-23 04:38:21 -0500

    cracker.py file changed

--- cracker.py
+++ cracker.py
@@ -2,3 +2,6 @@ from termcolor import cprint
 import hashlib
 from urllib.request import urlopen
 from datetime import datetime
+
+user_password = input("What password do you want to crack?")
+wordlist = 12
