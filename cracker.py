21f3923f6caa9207a780817ac9e83c13cda90027
Andrea Dybendal <anddyb671@gmail.com>
2023-05-23 04:52:46 -0500

    cracker.py file changed

--- cracker.py
+++ cracker.py
@@ -5,3 +5,16 @@ from datetime import datetime
 
 user_password = input("What password do you want to crack?")
 wordlist = 12
+
+password_1 = 'password'
+password_2 = 'crackingmewillbedifficult'
+password_3 = 'baileyiscute'
+password_4 = 'tryhackme'
+password_5 = 'iamafirewall'
+
+
+cprint("The string hash value is : " + str(hash(password_1)), 'green')
+cprint("The string hash value is : " + str(hash(password_2)), 'green')
+cprint("The string hash value is : " + str(hash(password_3)), 'green')
+cprint("The string hash value is : " + str(hash(password_4)), 'green')
+cprint("The string hash value is : " + str(hash(password_5)), 'green')
