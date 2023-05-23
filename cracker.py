6660392f3d9e040872813d22c54d0d2e5ea9a535
Andrea Dybendal <anddyb671@gmail.com>
2023-05-23 08:19:54 -0500

    Changed file and added more lines

--- cracker.py
+++ cracker.py
@@ -6,20 +6,32 @@ from datetime import datetime
 user_password = input("What password do you want to crack?: ")
 wordlist = 'wordlist.txt'
 
-password_1 = 'password'
-password_2 = 'crackingmewillbedifficult'
-password_3 = 'baileyiscute'
-password_4 = 'tryhackme'
-password_5 = 'iamafirewall'
+def crackHash(user_password):
+    try:
+        plist = open("wordlist.txt", "r")
+    except:
+        print("Could not find file")
+        
+    for password in plist:
+        encPass = password.encode("utf-8")
+        digest = hashlib.md5(encPass.strip()).hexdigest()
+        if digest == user_password:
+            print("Password Found: " + password)
 
-def hash(password):
-    result = hashlib.sha1(password.encode())
-    return result.hexdigest()
+#password_1 = 'password'
+#password_2 = 'crackingmewillbedifficult'
+#password_3 = 'baileyiscute'
+#password_4 = 'tryhackme'
+#password_5 = 'iamafirewall'
 
-cprint("The string hash value is : " + str(hash(password_1)), 'green')
-cprint("The string hash value is : " + str(hash(password_2)), 'green')
-cprint("The string hash value is : " + str(hash(password_3)), 'green')
-cprint("The string hash value is : " + str(hash(password_4)), 'green')
-cprint("The string hash value is : " + str(hash(password_5)), 'green')
+#def hash(password):
+    #result = hashlib.sha1(password.encode())
+    #return result.hexdigest()
+
+#cprint("The string hash value is : " + str(hash(password_1)), 'green')
+#cprint("The string hash value is : " + str(hash(password_2)), 'green')
+#cprint("The string hash value is : " + str(hash(password_3)), 'green')
+#cprint("The string hash value is : " + str(hash(password_4)), 'green')
+#cprint("The string hash value is : " + str(hash(password_5)), 'green')
 
 
