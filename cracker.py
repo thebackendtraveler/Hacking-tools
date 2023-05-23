762bc66def6197051200d227d87a79c41029a513
Andrea Dybendal <anddyb671@gmail.com>
2023-05-23 08:05:37 -0500

    cracker.py file changed

--- cracker.py
+++ cracker.py
@@ -12,9 +12,14 @@ password_3 = 'baileyiscute'
 password_4 = 'tryhackme'
 password_5 = 'iamafirewall'
 
+def hash(password):
+    result = hashlib.sha1(password.encode())
+    return result.hexdigest()
 
 cprint("The string hash value is : " + str(hash(password_1)), 'green')
 cprint("The string hash value is : " + str(hash(password_2)), 'green')
 cprint("The string hash value is : " + str(hash(password_3)), 'green')
 cprint("The string hash value is : " + str(hash(password_4)), 'green')
 cprint("The string hash value is : " + str(hash(password_5)), 'green')
+
+
