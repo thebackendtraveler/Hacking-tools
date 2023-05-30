a1db6fc79c2866e0393928681e85dfeb34212ad8
Andrea Dybendal <anddyb671@gmail.com>
2023-05-30 05:57:42 -0500

    Removed unused code

--- login_cracker.py
+++ login_cracker.py
@@ -38,13 +38,9 @@ try:
 
     data = {'username':username, 'password':password, "Login":'submit'}
     send_data_url = requests.post(url, data=data, verify=False)
-    cprint("The header: ", 'green')
-    print(send_data_url.headers)
-    cprint("The HTTP status code: ", 'green')
     print(send_data_url.status_code, ": HTTP status")
     cprint("The HTML: ", 'green')
     print(send_data_url.content)
-    cprint("The URL: ", 'green')
     print(send_data_url.url, ": The URL")
 
     if "Login failed" in str(send_data_url.content):
