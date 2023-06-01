93c91ff2df60ab0c83defa813aaedee4265a437c
Andrea Dybendal <anddyb671@gmail.com>
2023-06-01 06:59:39 -0500

    the textfile was changed

--- burster.py
+++ burster.py
@@ -6,7 +6,7 @@ from termcolor import cprint
 
 url = sys.argv[1]
 wordlist = sys.argv[2]
-ext = sys.argv[3]
+subdir = sys.argv[3]
 
 def write(word):
     f1 = open("write1.txt", "a")
@@ -15,14 +15,14 @@ def write(word):
 fo = open(wordlist, "r+")
 for i in range(2000):
     word = fo.readline(10).strip()
-    surl = url+word+ext
+    surl = url+word+subdir
     #print(surl)
 
     response = requests.get(surl)
     #print(response)
     if (response.status_code == 200):
-        cprint("[+] Found :- ", surl, 'green')
+        print("[+] Found :- ", surl)
         write(word)
     else:
-        cprint("[-] Not found :- ", surl, 'red')
+        print("[-] Not found :- ", surl)
         pass
