adcdaaa07c57885e2ff3d33aa6c55fc23ed2b264
Andrea Dybendal <anddyb671@gmail.com>
2023-06-09 10:42:57 -0500

    changed so user can input hash

--- password_cracker.py
+++ password_cracker.py
@@ -8,21 +8,8 @@ import sys
 banner = pyfiglet.figlet_format("RACCOON -> PCRACKER")
 cprint(banner, 'blue')
 
-
-#Asking the user for a string to hash and a wordlist
-hash = input("Enter a password to hash: ")
+#Asking the user for a wordlist
 wordlist = input("Enter a wordlist to use: ")
-
-#The banner with information about when the cracking starts + the hash to be cracked
-cprint("_" * 50, 'blue')
-cprint("Cracking password " + hash, 'blue')
-cprint("The wordlist is:  " + wordlist, 'blue')
-cprint("Password cracking started at: " + str(datetime.now()), 'blue')
-cprint("_" * 50, 'blue')
-
-print("\n") # Printing an empty line to make the output nicer
-cprint("MD5 cracking is starting...", 'green')
-
 try:
     def crackHash_md5(inputHash):
         """
@@ -32,12 +19,24 @@ try:
             This function will use the MD5 algorithm.
         """
    
+        #Asking the user for a  hash 
+        inputHash = input("Enter a hash: ")
+       
+        #The banner with information about when the cracking starts + the hash to be cracked
+        cprint("_" * 50, 'blue')
+        cprint("Cracking hash: " + inputHash, 'blue')
+        cprint("The wordlist is:  " + wordlist, 'blue')
+        cprint("Cracking hashes started at: " + str(datetime.now()), 'blue')
+        cprint("_" * 50, 'blue')
+
+        print("\n") # Printing an empty line to make the output nicer
+        cprint("MD5 cracking is starting...", 'green')
+
         passFile = open(wordlist, "r+")
         
-
         for password in passFile:
-            encHash = hash.encode("utf-8") # This line hashes what the user inputed
-            inputHash = hashlib.md5(encHash.strip()).hexdigest() # A digest for the input string is created
+            #encHash = hash.encode("utf-8") # This line hashes what the user inputed
+            #inputHash = hashlib.md5(encHash.strip()).hexdigest() # A digest for the input string is created
             encPass = password.encode("utf-8") # This code is hashing the wordlist passwords
             wordlistHash = hashlib.md5(encPass.strip()).hexdigest() # The program is using  md5 
 
@@ -51,18 +50,18 @@ try:
                 # This code runs when the input hash is the same as one of the wordlist hashes
                  cprint("Input hash: " + inputHash, 'white')
                  cprint("List hash : " + wordlistHash, 'white')
-                 cprint("SUCCESS!! Password Found: " + password, 'green')
+                 cprint("SUCCESS!! Password Found :) : " + password, 'green')
             else: 
                  cprint("Input hash: " + inputHash, 'white')
                  cprint("List hash : " + wordlistHash, 'white')
-                 cprint("FAIL!! Password Not Found: " + password, 'red')
+                 cprint("FAIL!! Password Not Found :( : " + password, 'red')
                  # If the hashes are not the same, the program prints that to the user, and continues
                  pass
     
     # This line is calling the crackHash_md5 function. The code will not run if this is removed.
     # The hash here acts as a placeholder, and it makes sure the input is correct
     if __name__ == '__main__':
-        crackHash_md5("d3eb05a3d5bb7e4901f739286ba8eee9")   
+        crackHash_md5("")   
 # Here is a complex exception statement that tells the program what to do when there is an error
 except KeyboardInterrupt:
         cprint('Quitting! THe program was terminated by the user', 'red')
@@ -82,12 +81,6 @@ except ValueError:
 
 print("\n")
 
-#Asking the user for a string to hash 
-hash = input("Enter a password to hash: ")
-print("\n")
-
-cprint("SHA256 cracking is starting...", 'green')
-
 try: 
     def crackHash_sha256(inputHash):
         """
@@ -96,18 +89,20 @@ try:
             Next, the program will compare the wordlist hash to the user input hash.
             This function will use the SHA256 algorithm. 
         """
-  
-        passFile = open(wordlist, "r")
         
+        # Asking the user for a hash to compare with the wordlist
+        inputHash = input("Enter a hash: ")
+        passFile = open(wordlist, "r")
+        print("\n")
+
+        cprint("SHA256 cracking is starting...", 'green')
    
         for password in passFile:
-            encHash = hash.encode("utf-8") # This line hashes what the user inputed
-            inputHash = hashlib.sha256(encHash.strip()).hexdigest() # Here a digest for the input string is created
-            #cprint("Input hash: " + inputHash, 'white')
+            #encHash = hash.encode("utf-8") # This line hashes what the user inputed
+            #inputHash = hashlib.sha256(encHash.strip()).hexdigest() # Here a digest for the input string is created
             encPass = password.encode("utf-8") # This code is hashing the plain text passwords
             wordlistHash = hashlib.sha256(encPass.strip()).hexdigest() # We are using the md5 hashing algorithm
-            #cprint("List hash : " + wordlistHash, 'white')
-
+        
             fdw = open("pcrack_results.txt", "+a") # Here the program is opening the output file in +a mode
             fdw.write("\n")
             fdw.write("Valid SHA256 hash: ")
@@ -118,18 +113,18 @@ try:
                  # This code runs when the input hash is the same as one of the wordlist hashes
                  cprint("Input hash: " + inputHash, 'white')
                  cprint("List hash : " + wordlistHash, 'white')
-                 cprint("SUCCESS!! Password Found: " + password, 'green')
+                 cprint("SUCCESS!! Password Found :) : " + password, 'green')
             else:
                  cprint("Input hash: " + inputHash, 'white')
                  cprint("List hash : " + wordlistHash, 'white')
-                 cprint("FAIL!! Password Not Found: " + password, 'red')
+                 cprint("FAIL!! Password Not Found :( : " + password, 'red')
                  # If the hashes are not the same, the program prints that to the user, and continues
                  pass
                 
     # This line is calling the crackHash function. The code will not run if this is removed.
     # The hash here is a placeholder, and it makes sure the input is a 256 hash
     if __name__ == '__main__':
-        crackHash_sha256("da088df0c22020eda7fef865007b2465bd6ed0fb8a47bdf10a75470ecca4559d")
+        crackHash_sha256("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
 # Here the program has instructions for what to do when it encounters an error
 except KeyboardInterrupt:
         cprint('Quitting! THe program was terminated by the user', 'red')
@@ -141,14 +136,7 @@ except FileExistsError:
         cprint("This file already exists. Try writing to another file...")
         sys.exit()
 
-
 print("\n")
-
-#Asking the user for a string to hash 
-hash = input("Enter a password to hash: ")
-print("\n")
-
-cprint("SHA512 cracking is starting...", 'green')
 
 try: 
     def crackHash_sha512(inputHash):
@@ -158,18 +146,20 @@ try:
             and compare it to the user input hash. 
             This function will use the sha512 algorithm.
         """
- 
-        passFile = open(wordlist, "r")
         
+        #Asking the user for a string to hash 
+        inputHash = input("Enter a hash: ")
+        passFile = open(wordlist, "r")
+        print("\n")
+        cprint("SHA512 cracking is starting...", 'green')
 
         for password in passFile:
-            encHash = hash.encode("utf-8") # This line hashes what the user inputed
-            inputHash = hashlib.sha512(encHash.strip()).hexdigest() # Here a digest for the input string is created
-            #cprint("Input hash: " + inputHash, 'white')
+            #encHash = hash.encode("utf-8") # This line hashes what the user inputed
+            #inputHash = hashlib.sha512(encHash.strip()).hexdigest() # Here a digest for the input string is created
+            
             encPass = password.encode("utf-8") # This code is hashing the plain text passwords
             wordlistHash = hashlib.sha512(encPass.strip()).hexdigest() # We are using the md5 hashing algorithm
-            #cprint("List hash : " + wordlistHash, 'white')
-
+            
             fdw = open("pcrack_results.txt", "+a") # The file is opened in +a to append more lines
             fdw.write("\n")
             fdw.write("Valid SHA 512 hash: ") # Then the program writes the results to a file
@@ -179,18 +169,18 @@ try:
                  # This code runs when the input hash is the same as one of the wordlist hashes
                  cprint("Input hash: " + inputHash, 'white')
                  cprint("List hash : " + wordlistHash, 'white')
-                 cprint("SUCCESS!! Password Found: " + password, 'green')
+                 cprint("SUCCESS!! Password Found :) : " + password, 'green')
             else: 
                  cprint("Input hash: " + inputHash, 'white')
                  cprint("List hash : " + wordlistHash, 'white')
-                 cprint("FAIL!! Password Not Found: " + password, 'red')
+                 cprint("FAIL!! Password Not Found :( : " + password, 'red')
                  # If the hashes are not the same, the program prints that to the user, and continues
                  pass
    
     # This line is calling the crackHash function. The code will not run if this is removed.
     # The hash here is a placeholder, and it makes sure the input for the program is a 512 hash
     if __name__ == '__main__':
-        crackHash_sha512("acb70dbd1969d70601892f3dd668919cfd2398fd440aaf8c6157620df352843ce5bd9d9dc3f167e6993b90a0d28f1015eb820e99a0625a2923376a71f4944925")
+        crackHash_sha512("9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043")
            
 except KeyboardInterrupt:
         cprint('Quitting! The program was interrupted by the user', 'red')   
